55c49a16506229038b703ee4e07053b0b99f2c25
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2015-05-06 15:39:29 +0200

    works locally

--- bluemix-java-spring-mongodb/src/main/java/be/craftworkz/demo/Application.java
+++ bluemix-java-spring-mongodb/src/main/java/be/craftworkz/demo/Application.java
@@ -3,10 +3,8 @@ package be.craftworkz.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.cloud.config.java.ServiceScan;
 
 @SpringBootApplication
-@ServiceScan
 @EnableMongoRepositories
 public class Application {
 
