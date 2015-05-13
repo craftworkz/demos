9eacda46a6b9d7ff590ab16e490ad56f07377647
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2015-05-13 15:03:13 +0200

    java executable example

---
+++ bluemix-java-executable/src/main/java/co/craftworkz/App.java
@@ -0,0 +1,12 @@
+package co.craftworkz;
+
+public class App {
+
+	public static void main(String[] args) throws Exception {
+		while(true) {
+			System.out.println("Hello World!");
+			Thread.sleep(5000);
+		}
+	}
+	
+}
