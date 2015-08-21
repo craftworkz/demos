e7fa3693035694b14650b88d340ef1c8c598ea26
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2015-08-21 13:02:08 +0200

    add liberty profile sample app

---
+++ bluemix-java-libertyprofile/src/main/java/co/craftworkz/HelloServlet.java
@@ -0,0 +1,15 @@
+package co.craftworkz;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		res.getOutputStream().print("Hello World!");
+	}
+	
+}
